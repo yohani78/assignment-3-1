6c9295dbc5a253abb773462e8bd6f6c670ecc0bf
yohani78 <yohani78@yahoo.com>
2015-10-12 23:43:09 +0000

    pushing

---
+++ loop.js
@@ -0,0 +1,37 @@
+
+//JavaScript File
+var text ="";
+/*var i = 0;
+/* loop 
+for ( i = 3; i < 5; i++) {
+    text += "The number is = " + i + "\n";
+}    
+ //   console.log(text);
+    
+    var text ="";
+var i = 0;
+    
+for ( i = 2; i < 15; i++) {
+    text += "The number is = " + i + "\n";
+}    
+    console.log(text);
+    
+
+
+var students = new Array("Gaby", "Yoh", "Tyler", "John", "Michaerl", "Giang");
+
+var x;
+for (x in students) {
+    text += students[x] + "\n"; // text = text + student[x] + "\n";
+    
+}
+console.log(text); 
+*/
+
+var i=0;
+while (i < 5) {
+    text += "the number is = " + i + "\n";
+    i++;
+}
+
+console.log(text);
