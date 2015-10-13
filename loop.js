1d78afecf0bbc4522447801f2ff5127f069a9c49
yohani78 <yohani78@yahoo.com>
2015-10-13 00:08:28 +0000

    pushing

--- loop.js
+++ loop.js
@@ -7,7 +7,7 @@ for ( i = 3; i < 5; i++) {
     text += "The number is = " + i + "\n";
 }    
  //   console.log(text);
-    
+ ----------------------------------   
     var text ="";
 var i = 0;
     
@@ -16,7 +16,7 @@ for ( i = 2; i < 15; i++) {
 }    
     console.log(text);
     
-
+----------------------------------------------
 
 var students = new Array("Gaby", "Yoh", "Tyler", "John", "Michaerl", "Giang");
 
@@ -26,8 +26,8 @@ for (x in students) {
     
 }
 console.log(text); 
-*/
 
+-----------------------------------------------
 var i=0;
 while (i < 5) {
     text += "the number is = " + i + "\n";
@@ -35,3 +35,31 @@ while (i < 5) {
 }
 
 console.log(text);
+
+--------------------------------------------
+var i=99;
+var value = "i is greater thant 15"
+ do {
+        text += "the number is = " + i + "";
+        i++;
+    }
+    
+while (i < 15)
+
+//if (i>15) console.log(value); 
+
+console.log(text + ",  and " + value);
+---------------------------------- */
+var number = 1;
+var sum = 0;
+while(true) 
+{
+    sum += number;
+    if (number == 5){
+        break;// break out of the loop if number == 5 and execute console.log
+        
+    }
+    number++;
+    
+}
+console.log("sum = " + sum);
